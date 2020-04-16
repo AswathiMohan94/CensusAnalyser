f4b3a49f13391c7404a9c63b7a339ff9bac0c37f
AswathiMohan94 <60881180+AswathiMohan94@users.noreply.github.com>
2020-04-16 18:34:41 +0530

    [Aswathi] refactored the code

--- src/test/java/CensusAnalyserDummyTest/CensusAnalyserTest.java
+++ src/test/java/CensusAnalyserDummyTest/CensusAnalyserTest.java
@@ -125,33 +125,3 @@ public class CensusAnalyserTest {
     }
 }
 
- /*   @Test
-    public void givenIndianCensusData_WhenSortedPopulation_ShouldReturnSortedResult() throws CensusAnalyserException {
-        CensusAnalyser censusAnalyser = new CensusAnalyser();
-        censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
-        String sortedCensusData = null;
-        try {
-            sortedCensusData = censusAnalyser.getPopulationWiseSortedCensusData();
-        } catch (CensusAnalyserException e) {
-            e.printStackTrace();
-        }
-        IndiaCensusCSV[] Census = new Gson().fromJson(sortedCensusData, IndiaCensusCSV[].class);
-        //   System.out.println(sortedCensusData);
-        int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
-        Assert.assertEquals(29, numOfRecords);
-    }
-
-    @Test
-    public void givenIndianCensusData_WhenSortedPopulationDensity_ShouldReturnSortedResult() throws CensusAnalyserException {
-        CensusAnalyser censusAnalyser = new CensusAnalyser();
-        censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
-        String sortedCensusData = null;
-        try {
-            sortedCensusData = censusAnalyser.getDensityWiseSortedCensusData();
-        } catch (CensusAnalyserException e) {
-            e.printStackTrace();
-        }
-        IndiaCensusCSV[] Census = new Gson().fromJson(sortedCensusData, IndiaCensusCSV[].class);
-        //   System.out.println(sortedCensusData);
-    }*/
-
